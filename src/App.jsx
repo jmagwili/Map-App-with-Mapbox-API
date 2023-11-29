be5760c94828eff0a634e2f208cdae7460619dbc
jmagwili <joshuamagwili@gmail.com>
2023-11-29 17:30:49 +0800

    updated viewport state

--- src/App.jsx
+++ src/App.jsx
@@ -9,69 +9,58 @@ import { FaLocationDot } from "react-icons/fa6";
 const token = import.meta.env.VITE_MAPBOX_TOKEN
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [longitude, setLongitude] = useState(0); 
+  const [longitude, setLongitude] = useState(0);
   const [latitude, setLatitude] = useState(0);
-  const [newPlace, setNewPlace] = useState(null) 
-  
-  
-  useEffect(()=>{
+  const [viewport, setViewport] = useState({
+    longitude: 0,
+    latitude: 0,
+    zoom: 15,
+  });
+
+  useEffect(() => {
     navigator.geolocation.getCurrentPosition(
-      (position)=>{
-      
+      (position) => {
         setLatitude(position.coords.latitude);
         setLongitude(position.coords.longitude);
-        console.log(latitude,longitude)
-    },
-      (error)=>{
-        console.log(error)
-      }, 
-      {
-        enableHighAccuracy: true
-      }) 
-  })
+        setViewport({
+          ...viewport,
+          longitude: position.coords.longitude,
+          latitude: position.coords.latitude,
+        });
+      },
+      (error) => console.log(error),
+      { enableHighAccuracy: true }
+    );
+  }, []);
 
-  const [viewport, changeViewPort] = useState({
-    longitude: longitude,
-    latitude: latitude,
-    zoom: 8
-})
-  
   return (
     <> 
       <div style={{width: '100vw',height: '100vh', margin: "solid red 20px" }}>
       <ReactMapGL 
-      //{...viewport}
-      longitude={longitude}
-      latitude={latitude}
-      zoom={15} 
-      mapboxAccessToken={token}
-      width = "100%"
-      height = "100%"
-      mapStyle="mapbox://styles/mapbox/streets-v12"
-      
+        {...viewport}
+        mapboxAccessToken={token}
+        width = "100%"
+        height = "100%"
+        mapStyle="mapbox://styles/mapbox/streets-v12"
       >
         <Marker
-              latitude={latitude}
-              longitude={longitude}
-              offsetLeft={-3.5 * viewport.zoom}
-              offsetRight={-7 * viewport.zoom}
-              draggable={true}
-              onDragEnd={(e)=>{
-                console.log(e.lngLat.lng,e.lngLat.lat)
+          latitude={latitude}
+          longitude={longitude}
+          offsetLeft={-3.5 * viewport.zoom}
+          offsetRight={-7 * viewport.zoom}
+          draggable={true}
+          onDragEnd={e=>console.log(e.lngLat.lng,e.lngLat.lat)}
+        >
+          <div>
+            <FaLocationDot 
+              style={{
+                height: "40px",
+                width:"auto",
+                color:"red"
               }}
-            >
-              <div>
-                <FaLocationDot 
-                  style={{
-                    height: "40px",
-                    width:"auto",
-                    color:"red"
-                  }}
-                />
-
-                </div>
-            </Marker>
+            />
+          </div>
+        </Marker>
       </ReactMapGL>
       </div>
     </>
