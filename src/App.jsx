2bbf015f7795b01066cf3592101a97ccfe28f863
shannonjohn <shanjohncantz@gmail.com>
2023-12-02 00:27:06 +0800

    tested multiple coords w/popups

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
-import { useState, useEffect, useRef } from 'react';
-import mapboxgl from 'mapbox-gl';
+import { useState, useEffect, useRef } from "react";
 
-import ReactMapGL, { Marker, Popup } from 'react-map-gl'
-import './App.css';
-import 'mapbox-gl/dist/mapbox-gl.css';
+import ReactMapGL, { Marker, Popup } from "react-map-gl";
+import "./App.css";
+import "mapbox-gl/dist/mapbox-gl.css";
 import { FaLocationDot } from "react-icons/fa6";
 
-const token = import.meta.env.VITE_MAPBOX_TOKEN
+const token = import.meta.env.VITE_MAPBOX_TOKEN;
 
 function App() {
-  const [newPlace, setNewPlace] = useState(null)
+  const [newPlace, setNewPlace] = useState(null);
   const [longitude, setLongitude] = useState(0);
   const [latitude, setLatitude] = useState(0);
   const [viewport, setViewport] = useState({
@@ -18,9 +17,40 @@ function App() {
     zoom: 15,
   });
 
-  const [showPopup,setShowPopup] = useState(false);
-  
+  const [mockApiData, setMockApiData] = useState([
+    {
+      username: "Shannon",
+      coords: [120.9581200219007, 14.31781346759307],
+    },
+    {
+      username: "Joshua Manalo",
+      coords: [120.9650748167237, 14.336595493481354],
+    },
+    {
+      username: "Jonel Belaro Baqar",
+      coords: [120.98195064088475, 14.334375074985957],
+    },
+    {
+      username: "Garret Cold Sucker",
+      coords: [120.97472806593458, 14.328251988859591],
+    },
+    {
+      username: "Johnny sins",
+      coords: [120.95176195520145, 14.325773806431215],
+    },
+    {
+      username: "Sample Popup",
+      coords: [120.95120637251404, 14.318910292569015],
+    },
+    {
+      username: "Mantos Jonel",
+      coords: [120.96523483539562, 14.319919646007861],
+    },
+  ]);
+  console.log(mockApiData[0].coords[0]);
 
+  const [showPopup, setShowPopup] = useState(false);
+  const [selectedMarker, setSelectedMarker] = useState(null);
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(
       (position) => {
@@ -37,75 +67,123 @@ function App() {
     );
   }, []);
 
-
-  const onClickMarker = () => {
-    setShowPopup(true);
-  }
+  const onClickMarker = (index) => {
+    setSelectedMarker(index);
+  };
   const closePopup = () => {
     setShowPopup(false);
+    setSelectedMarker(null);
   };
 
   return (
-    <> 
-      <div style={{width: '100vw',height: '100vh', margin: "solid red 20px" }}>
-      <ReactMapGL 
-        {...viewport}
-        mapboxAccessToken={token}
-        width = "100%"
-        height = "100%"
-        mapStyle="mapbox://styles/mapbox/streets-v12"
-        interactive = {true}
-        onDrag={(e)=>{
-          setViewport({
-            longitude: e.viewState.longitude,
-            latitude: e.viewState.latitude
-          })
-        }}
-      >
+    <>
+      <div style={{ width: "50vw", height: "50vh" }}>
+        <ReactMapGL
+          {...viewport}
+          mapboxAccessToken={token}
+          width="100%"
+          height="100%"
+          mapStyle="mapbox://styles/mapbox/streets-v12"
+          interactive={true}
+          onDrag={(e) => {
+            setViewport({
+              longitude: e.viewState.longitude,
+              latitude: e.viewState.latitude,
+            });
+          }}
+        >
+          <Marker
+            latitude={latitude}
+            longitude={longitude}
+            offsetLeft={-3.5 * viewport.zoom}
+            offsetRight={-7 * viewport.zoom}
+            draggable={true}
+            onDragEnd={(e) => {
+              setLatitude(e.lngLat.lat);
+              setLongitude(e.lngLat.lng);
+              console.log(e.lngLat.lat);
+              console.log(e.lngLat.lng);
+            }}
+            onClick={onClickMarker}
+          >
+            <div>
+              <FaLocationDot
+                style={{
+                  height: "40px",
+                  width: "auto",
+                  color: "red",
+                }}
+              />
+            </div>
+          </Marker>
 
-            <Marker
+          {showPopup && (
+            <Popup
               latitude={latitude}
               longitude={longitude}
-              offsetLeft={-3.5 * viewport.zoom}
-              offsetRight={-7 * viewport.zoom}
-              draggable={true}
-              onDragEnd={e=>{
-                setLatitude(e.lngLat.lat)
-                setLongitude(e.lngLat.lng)
-                console.log(e.lngLat.lat)
-                console.log(e.lngLat.lng)
+              offset={{
+                "bottom-left": [12, -38],
+                bottom: [0, -38],
+                "bottom-right": [-12, -38],
               }}
-              onClick={onClickMarker}
+              closeButton={true}
+              closeOnClick={false}
+              anchor="top"
+              onClose={closePopup}
+            >
+              <h1>Popup</h1>
+            </Popup>
+          )}
+        </ReactMapGL>
+      </div>
+      <h2>Multiple Locations</h2>
+      <div style={{ width: "50vw", height: "50vh", marginTop: "50px" }}>
+        <ReactMapGL
+          mapboxAccessToken={token}
+          width="100%"
+          height="100%"
+          mapStyle="mapbox://styles/mapbox/streets-v12"
+          interactive={true}
+          center={mockApiData[0].coords}
+        >
+          {mockApiData.map((customer, index) => (
+            <Marker
+              key={index}
+              latitude={customer.coords[1]}
+              longitude={customer.coords[0]}
+              draggable={true}
+              onClick={() => onClickMarker(index)}
             >
               <div>
-                <FaLocationDot 
+                <FaLocationDot
                   style={{
                     height: "40px",
-                    width:"auto",
-                    color:"red"
+                    width: "auto",
+                    color: "red",
                   }}
                 />
               </div>
             </Marker>
-
-            {showPopup && (<Popup
-              latitude={latitude}
-              longitude={longitude}
+          ))}
+          {selectedMarker !== null && (
+            <Popup
+              latitude={mockApiData[selectedMarker].coords[1]}
+              longitude={mockApiData[selectedMarker].coords[0]}
               offset={{
-                'bottom-left': [12, -38],  'bottom': [0, -38], 'bottom-right': [-12, -38]
+                "bottom-left": [12, -38],
+                bottom: [0, -38],
+                "bottom-right": [-12, -38],
               }}
               closeButton={true}
               closeOnClick={false}
-              anchor='top'
+              anchor="top"
               onClose={closePopup}
-              >
-              <h1>Popup</h1>
-            </Popup>)}
-
-            
-
-        
-      </ReactMapGL>
+              
+            >
+              <h2>{mockApiData[selectedMarker].username}</h2>
+            </Popup>
+          )}
+        </ReactMapGL>
       </div>
     </>
   );
